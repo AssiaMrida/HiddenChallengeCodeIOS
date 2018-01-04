9b78fb77ef867d4d4b8ae765bc42fe3b899e23fe
Assia Mrida <assia.mrida@gmail.com>
2018-01-04 14:52:28 +0000

    configurer tableView & modifie ViewController & modifie DetailsViewController & modifie TableViewCell

--- HiddenChallengeCodeIOS/DetailViewController.swift
+++ HiddenChallengeCodeIOS/DetailViewController.swift
@@ -9,27 +9,39 @@
 import UIKit
 
 class DetailViewController: UIViewController {
-
+    //current controller IBOutlets
+  
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var desLabel: UILabel!
+    @IBOutlet weak var loginLabel: UILabel!
+    @IBOutlet weak var startLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    //data from previous controller
+    var nameString:String!
+    var desString:String!
+    var imageString:String!
+    var startInt:String!
+    var loginString:String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.updateUI()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func updateUI() {
+        self.nameLabel.text = nameString
+        self.desLabel.text = desString
+        self.loginLabel.text = loginString
+        self.startLabel.text = startInt
+        let imgURL = URL(string:imageString)
+        let data = NSData(contentsOf: (imgURL)!)
+        self.imageView.image = UIImage(data: data! as Data)
     }
-    */
 
 }
--- HiddenChallengeCodeIOS/ViewController.swift
+++ HiddenChallengeCodeIOS/ViewController.swift
@@ -8,18 +8,69 @@
 
 import UIKit
 
-class ViewController: UIViewController{
+class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
+    
+    
 
+
+    @IBOutlet weak var tableView: UITableView!
+    var nameArray = [String]()
+    var desArray = [Any]()
+    var loginArray = [String]()
+    var startArray = [Double]()
+    var imgURLArray = [String]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+    
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return nameArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as! TableViewCell
+        cell.nameLabel.text = nameArray[indexPath.row]
+        
+        cell.desLabel.text = desArray[indexPath.row] as? String
+        cell.loginLabel.text = loginArray[indexPath.row]
+        
+        cell.startLabel.text = String(format:"%.1f", startArray[indexPath.row])+"K"
+        
+        
+        let imgURL = NSURL(string: imgURLArray[indexPath.row])
+        
+        if imgURL != nil {
+            let data = NSData(contentsOf: (imgURL as URL?)!)
+            cell.imgView.image = UIImage(data: data! as Data)
+        }
+        
+        return cell
+    }
+    
+    ///for showing next detailed screen with the downloaded info
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        let vc = self.storyboard?.instantiateViewController(withIdentifier: "DetailViewController") as! DetailViewController
+        vc.imageString = imgURLArray[indexPath.row]
+        vc.nameString = nameArray[indexPath.row]
+        vc.loginString = loginArray[indexPath.row]
+        vc.startInt = String(format:"%.1f", startArray[indexPath.row])+"K"
+        vc.desString = desArray[indexPath.row] as! String
+        
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
 }
 
