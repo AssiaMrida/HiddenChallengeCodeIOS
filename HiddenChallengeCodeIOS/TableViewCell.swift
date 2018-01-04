7746e86fe46c6f8de6e52a4f7e094df6a709ac2a
Assia Mrida <assia.mrida@gmail.com>
2018-01-04 14:22:12 +0000

    add new file TableViewCell & DetailViewController

---
+++ HiddenChallengeCodeIOS/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  HiddenChallengeCodeIOS
+//
+//  Created by mac on 04/01/2018.
+//  Copyright © 2018 mac. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
