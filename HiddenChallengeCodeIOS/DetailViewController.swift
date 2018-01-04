54309909390df4ae344195dde3c2cb47e4d10cec
Assia Mrida <assia.mrida@gmail.com>
2018-01-04 14:02:21 +0000

    initial commit

---
+++ HiddenChallengeCodeIOS/DetailViewController.swift
@@ -0,0 +1,26 @@
+//
+//  DetailViewController.swift
+//  HiddenChallengeCodeIOS
+//
+//  Created by mac on 04/01/2018.
+//  Copyright © 2018 mac. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+}
